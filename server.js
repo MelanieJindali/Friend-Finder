a092e5ca66d26e4c60d4abe6389d6ab4b44b2919
Melanie Jindali <melaniejindali.dm@gmail.com>
2020-05-22 11:13:18 -0500

    Require express, routes, & start server

--- server.js
+++ server.js
@@ -1,2 +1,18 @@
-var express = require("express");
-var path = require("path")
+var express = require("express"); // Dependencies
+
+var PORT = process.env.PORT || 9000; // Port
+
+var app = express(); // Create express server
+
+// These handles data parsing
+app.use(express.urlencoded({ extended: true })); 
+app.use(express.json()); 
+
+// Route files
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+// Start server
+app.listen(PORT, function() {
+    console.log(`App listening on PORT: ${PORT}`)
+})
