b5d1fd8e26963484fee9c95226b5d352f9a15a93
Melanie Jindali <melaniejindali.dm@gmail.com>
2020-05-22 11:39:53 -0500

    Created route to default to homepage

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -6,4 +6,9 @@ module.exports = function(app) {
     app.get("/survey", function(req, res) {
         res.sendFile(path.join(__dirname, "../public/survey.html"));
     });
+
+    // Default to homepage
+    app.get("*", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/home.html"));
+    });
 }
