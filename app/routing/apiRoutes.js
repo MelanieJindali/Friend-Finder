2896fbfd4f6ae385a521724d5e753006f5389a71
Melanie Jindali <melaniejindali.dm@gmail.com>
2020-05-24 19:42:41 -0500

    Add POST method

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,7 +2,13 @@ var friends = require("../data/friends"); // Load Data
 
 module.exports = function(app) {
     // API GET requests
-    app.get("/api/friends", function (req, res) {
+    app.get("/api/friends", function(req, res) {
         res.json(friends);
+    });
+
+    // API POST requests
+    app.post("/api/friends", function(req, res) {
+        friends.push(req.body);
     })
 }
+
