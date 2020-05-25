ddce46e2a3f6f576040a4e98ad0176f117cb02d0
Melanie Jindali <melaniejindali.dm@gmail.com>
2020-05-25 13:13:00 -0500

    Add url to port

--- server.js
+++ server.js
@@ -14,5 +14,5 @@ require("./app/routing/htmlRoutes")(app);
 
 // Start server
 app.listen(PORT, function() {
-    console.log(`App listening on PORT: ${PORT}`)
+    console.log(`App listening on PORT: http://localhost:${PORT}`)
 })
