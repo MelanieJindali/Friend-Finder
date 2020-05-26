377ba0a02e78f9b9a41635f591be01c4b46ea0c9
Melanie Jindali <melaniejindali.dm@gmail.com>
2020-05-26 09:07:45 -0500

    Add more friends to array

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,19 +1,51 @@
 // Friend Data - dummy
-var friends = {
-    name: "Ahmed",
-    photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+var friends = [{
+    name: "Leslie",
+    photo: "https://upload.wikimedia.org/wikipedia/en/thumb/3/34/Leslie_Knope_%28played_by_Amy_Poehler%29.png/240px-Leslie_Knope_%28played_by_Amy_Poehler%29.png",
     scores:[
       5,
-      1,
-      4,
-      4,
+      5,
+      5,
+      5,
+      5,
+      5,
+      5,
       5,
       1,
-      2,
-      5,
-      4,
       1
     ]
-}
+},
+{
+  name: "April",
+  photo: "https://vignette.wikia.nocookie.net/parksandrecreation/images/e/ed/April-0.png/revision/latest/top-crop/width/360/height/360?cb=20180116070637",
+  scores:[
+    1,
+    1,
+    1,
+    5,
+    2,
+    5,
+    1,
+    1,
+    5,
+    5
+  ]   
+},
+{
+  name: "Tom",
+  photo: "https://cdn.costumewall.com/wp-content/uploads/2018/10/tom-haverford.jpg",
+  scores:[
+    5,
+    5,
+    5,
+    1,
+    5,
+    5,
+    5,
+    2,
+    1,
+    2
+  ]
+}]
 
 module.exports = friends;
