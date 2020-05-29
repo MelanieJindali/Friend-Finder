ac4e6013b3ddce2314a4270e8b62eb6688659669
Melanie Jindali <melaniejindali.dm@gmail.com>
2020-05-29 16:23:28 -0500

    Change if stmnt for difference

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -9,24 +9,24 @@ module.exports = function(app) {
     // API POST requests
     app.post("/api/friends", function(req, res) {
         var newScores = req.body.scores;
-        var bestie = 0; // Initial index
-        var minDiff = 1000; // Initial value for comparison
+        var bestieNm = '';
+        var bestieImg = '';
+        var totalDiff = 50; // Initial value for comparison
 
 
         // Go through friends list
         for (var i = 0; i < friends.length; i++) {
-            var totalDiff = 0;
+            var difference = 0;
         
             // Compare scores
             for (var j = 0; j < newScores.length; j++) {
-                totalDiff += (Math.abs(parseInt(friends[i].scores[j])) - parseInt((newScores[j])));
+                difference += (Math.abs(parseInt(friends[i].scores[j])) - parseInt((newScores[j])));
             };
 
-            // Setting new vals to variables for the next iterated comparison
-            if (totalDiff < minDiff) {
-                var bestieNm = friends[i].name;
-                var bestieImg = friends[i].photo;
-                minDiff = totalDiff;
+            if (difference < totalDiff) {
+                bestieNm = friends[i].name;
+                bestieImg = friends[i].photo;
+                totalDiff = difference;
             };
         };
 
